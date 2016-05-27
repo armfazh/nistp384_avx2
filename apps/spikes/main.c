d62966f288d7980b8eadaaaa547c5ad25ff81976
armfazh <armfazh@gmail.com>
2016-05-27 15:03:49 -0300

    Makefile

--- apps/spikes/main.c
+++ apps/spikes/main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+#include "element_1w_h0h7.h"
 
 int main()
 {
 	printf("=== MAIN === \n");
+
+
+#if 1 /* === Element 1w ===  */
+	printf("=== Element 1w === \n");
+
+	Element_1w_H0H7 a,b,c;
+	random_Element_1w_h0h7(a);
+	random_Element_1w_h0h7(b);
+
+	print_Element_1w_h0h7(a);
+	print_Element_1w_h0h7(b);
+
+	add_Element_1w_h0h7(c,a,b);
+	print_Element_1w_h0h7(c);
+
+#endif
+
 	return 0;
 }
