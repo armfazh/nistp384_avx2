e025b64c0a0c4c571e99661526b29b41a29a7c8c
armfazh <armfazh@gmail.com>
2016-06-17 19:00:20 -0300

    FIPS flag must be taken into account.

--- apps/spikes/ssl.c
+++ apps/spikes/ssl.c
@@ -3,12 +3,25 @@
 //
 #include <openssl/ec.h>
 #include <openssl/ecdh.h>
+#include <openssl/sha.h>
 #include <openssl/ecdsa.h>
 #include<openssl/objects.h>
 #include "../bench/clocks.h"
 
 #define P256 NID_X9_62_prime256v1
 #define P384 NID_secp384r1
+static void *KDF1_SHA1(const void *in, size_t inlen, void *out, size_t *outlen)
+	{
+#ifndef OPENSSL_NO_SHA
+	if (*outlen < SHA_DIGEST_LENGTH)
+		return NULL;
+	else
+		*outlen = SHA_DIGEST_LENGTH;
+	return SHA1(in, inlen, out);
+#else
+	return NULL;
+#endif	/* OPENSSL_NO_SHA */
+	}
 
 void print_point_proy(EC_GROUP*ec_group,EC_POINT*P)
 {
@@ -64,7 +77,8 @@ int main()
 //	printf("compiled with: %s\n", SSLeay_version(SSLEAY_CFLAGS));
 //	printf("built on: %s\n", SSLeay_version(SSLEAY_BUILT_ON));
 //	printf("located on: %s\n", SSLeay_version(SSLEAY_DIR));
-
+	FIPS_mode_set(1);
+	printf("OpenSSL FIPS: %s\n", FIPS_mode()?"On":"Off");
 
 //	printf("ECC Key type: %s\n", OBJ_nid2sn(P384));
 	ec_key = EC_KEY_new_by_curve_name(P384);
@@ -109,6 +123,8 @@ int main()
 
 	EC_KEY_generate_key(ec_key);
 	EC_KEY_generate_key(alice_key);
+	EC_KEY_set_asn1_flag(ec_key, OPENSSL_EC_NAMED_CURVE);
+	EC_KEY_set_asn1_flag(alice_key, OPENSSL_EC_NAMED_CURVE);
 	unsigned char *shared_secret;
 	int field_size = EC_GROUP_get_degree(ec_group);
 	int size = (field_size+7)/8;
@@ -116,37 +132,44 @@ int main()
 	ECDH_compute_key(shared_secret,size,EC_KEY_get0_public_key(ec_key),alice_key,NULL);
 
 	print_str_bytes(shared_secret);
-	EC_KEY_set_asn1_flag(ec_key, OPENSSL_EC_NAMED_CURVE);
-	EC_KEY_set_asn1_flag(alice_key, OPENSSL_EC_NAMED_CURVE);
-//	goto end;
 
+	ECDSA_SIG *sig=NULL;
+	char message [128];
 	long BENCH=200;
+
+
 	printf("Variable point:\n");
 	CLOCKS_RANDOM(
 			BN_rand_range(k, ec_order),
-			EC_POINT_mul(ec_group,kG,NULL,kG,k,NULL);
-			EC_POINT_get_affine_coordinates_GFp(ec_group,kG,n,m,NULL)
+			EC_POINT_mul(ec_group, kG, NULL, kG, k, NULL);
+					EC_POINT_get_affine_coordinates_GFp(ec_group, kG, n, m, NULL)
 	);
 
 	printf("Fixed point:\n");
 	CLOCKS_RANDOM(
 			BN_rand_range(k, ec_order),
-			EC_POINT_mul(ec_group,kG,k,NULL,NULL,NULL);
-			EC_POINT_get_affine_coordinates_GFp(ec_group,kG,n,m,NULL)
+			EC_POINT_mul(ec_group, kG, k, NULL, NULL, NULL);
+					EC_POINT_get_affine_coordinates_GFp(ec_group, kG, n, m, NULL)
+	);
+
+	oper_second(
+			BN_rand_range(k, ec_order),
+			EC_POINT_mul(ec_group, kG, k, NULL, NULL, NULL);
+					EC_POINT_get_affine_coordinates_GFp(ec_group, kG, n, m, NULL)
 	);
 
 	printf("Double point:\n");
 	CLOCKS_RANDOM(
 			BN_rand_range(k, ec_order),
-			EC_POINT_mul(ec_group,kG,k,_3G,m,NULL);
-			EC_POINT_get_affine_coordinates_GFp(ec_group,kG,n,m,NULL)
+			EC_POINT_mul(ec_group, kG, k, _3G, m, NULL);
+					EC_POINT_get_affine_coordinates_GFp(ec_group, kG, n, m, NULL)
 	);
 
+	EC_POINT *pubKey = EC_KEY_get0_public_key(ec_key);
 	printf("ECDH Key:\n");
-	CLOCKS(ECDH_compute_key(shared_secret,size,EC_KEY_get0_public_key(ec_key),alice_key,NULL));
-
-	ECDSA_SIG *sig=NULL;
-	char message [128];
+	CLOCKS(
+			ECDH_compute_key(shared_secret, size, pubKey, alice_key, KDF1_SHA1)
+	);
 	printf("ECDSA Sign:\n");
 	CLOCKS(
 			sig = ECDSA_do_sign(message,128,ec_key);
@@ -159,6 +182,8 @@ int main()
 			ECDSA_do_verify(message, 128, sig, ec_key);
 	);
 	ECDSA_SIG_free(sig);
+
+
 end:
 	free(shared_secret);
 	BN_free(n);
