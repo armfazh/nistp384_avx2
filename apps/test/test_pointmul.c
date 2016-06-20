d6278c42abd394508a2c06e365cb19cdf5bfd8e2
armfazh <armfazh@gmail.com>
2016-06-20 15:29:26 -0300

    Testing point multiplication, some issues must be debugged.

--- apps/test/test_pointmul.c
+++ apps/test/test_pointmul.c
@@ -5,7 +5,6 @@
 #include "test.h"
 #include "pointmul.h"
 #include <stdio.h>
-#include <string.h>
 
 #include <openssl/bn.h>
 #include <openssl/ec.h>
@@ -19,10 +18,12 @@
 
 void to_littleEndian(uint8_t *str_num, BIGNUM* n)
 {
-	BN_bn2bin(n,str_num);
 	int i;
 	uint8_t tmp;
 	uint8_t end = BN_num_bytes(n);
+
+	BN_bn2bin(n,str_num);
+
 	for(i=0;i<(end+1)/2 ;i++)
 	{
 		tmp  = str_num[i];
@@ -33,6 +34,21 @@ void to_littleEndian(uint8_t *str_num, BIGNUM* n)
 	{
 		str_num[i] = 0;
 	}
+}
+void to_point(Point_XY_1way * P,const EC_GROUP*ec_group, EC_POINT * OSSL_P)
+{
+	STR_BYTES str_x,str_y;
+	Element_1w_H0H7 x,y;
+	BIGNUM * Px = BN_new();
+	BIGNUM * Py = BN_new();
+	EC_POINT_get_affine_coordinates_GFp(ec_group,OSSL_P,Px,Py,NULL);
+	to_littleEndian(str_x,Px);
+	to_littleEndian(str_y,Py);
+	str_bytes_To_Element_1w_h0h7(x,str_x);
+	str_bytes_To_Element_1w_h0h7(y,str_y);
+	interleave(P->XY,x,y);
+	BN_free(Px);
+	BN_free(Py);
 }
 
 int compare_element(BIGNUM* x , uint8_t *y)
@@ -70,8 +86,6 @@ void test_ecc()
 	getGenerator(&P);
 	EC_KEY * ec_key = EC_KEY_new_by_curve_name(P384);
 	const EC_GROUP * ec_group = EC_KEY_get0_group(ec_key);
-	BIGNUM * ec_order = BN_new();
-	EC_GROUP_get_order(ec_group,ec_order,NULL);
 	const EC_POINT * OSSL_G = EC_GROUP_get0_generator(ec_group);
 	EC_POINT * OSSL_2G = EC_POINT_new(ec_group);
 	EC_POINT * OSSL_3G = EC_POINT_new(ec_group);
@@ -96,30 +110,30 @@ void test_ecc()
 
 	EC_POINT_free(OSSL_2G);
 	EC_POINT_free(OSSL_3G);
-	BN_free(ec_order);
 	EC_KEY_free(ec_key);
 }
 
 void test_pointmul()
 {
-	int cnt = 0;
+	long int cnt = 0;
 	int i,test;
 
-	Point_XY_1way G,kG;
-	getGenerator(&G);
+	Point_XY_1way P,kG;
 
 	EC_KEY * ec_key = EC_KEY_new_by_curve_name(P384);
 	const EC_GROUP * ec_group = EC_KEY_get0_group(ec_key);
 	BIGNUM * ec_order = BN_new();
 	EC_GROUP_get_order(ec_group,ec_order,NULL);
-//	const EC_POINT * OSSL_G = EC_GROUP_get0_generator(ec_group);
 
 	/* Fixed point multiplication */
 	EC_POINT * OSSL_kG = EC_POINT_new(ec_group);
+	EC_POINT * OSSL_Q  = EC_POINT_new(ec_group);
 	BIGNUM * k = BN_new();
-	STR_BYTES str_k;
+	BIGNUM * k1 = BN_new();
+	STR_BYTES str_k,str_k1;
 
 	printf("Fixed point test:");
+	cnt = 0;
 	for(i=0;i<TEST_TIMES;i++)
 	{
 		BN_rand_range(k,ec_order);
@@ -130,22 +144,91 @@ void test_pointmul()
 		test = compare_point(ec_group,OSSL_kG,&kG);
 		if(!test)
 		{
+			char* s=NULL;
 			STR_BYTES buf;
-			BN_print_fp(stdout,k);printf("\n");
 			to_littleEndian(buf,k);
-			printf("\nOP_k: \n");print_str_bytes(buf);
+			printf("\nOP_k: \n");printf("\n");print_str_bytes(buf);
 			printf("\nTH_k: \n");print_str_bytes(str_k);
 			print_affine_1way(&kG);
-			printf("%s\n",EC_POINT_point2hex(ec_group,OSSL_kG,POINT_CONVERSION_UNCOMPRESSED,NULL));
+			printf("%s\n",s=EC_POINT_point2hex(ec_group,OSSL_kG,POINT_CONVERSION_UNCOMPRESSED,NULL)); OPENSSL_free(s);
 			break;
 		}
 		cnt += test;
 	}
 	printf(" %ld %s\n",cnt , cnt == TEST_TIMES? "OK" : "FAIL" );
 
+	/* Variable point multiplication */
+	printf("Variable point test:");
+	BN_rand_range(k,ec_order);
+	EC_POINT_mul(ec_group,OSSL_kG,k,NULL,NULL,NULL);
+	cnt = 0;
+	for(i=0;i<TEST_TIMES;i++)
+	{
+		BN_rand_range(k,ec_order);
+		EC_POINT_mul(ec_group,OSSL_Q,NULL,OSSL_kG,k,NULL);
+
+		to_littleEndian(str_k,k);
+		to_point(&P,ec_group,OSSL_kG);
+		variable_point_multiplication(&kG,str_k,&P);
+		test = compare_point(ec_group,OSSL_Q,&kG);
+		if(!test)
+		{
+			STR_BYTES buf;
+			char* s = NULL;
+			to_littleEndian(buf,k);
+			printf("TH_k : \n");print_str_bytes(str_k);
+			printf("TH_P : \n");print_affine_1way(&P);
+			printf("TH_kP: \n");print_affine_1way(&kG);
+			printf("OP_k : \n");print_str_bytes(buf);
+			printf("OP_P : \n");printf("%s\n",s=EC_POINT_point2hex(ec_group,OSSL_kG,POINT_CONVERSION_UNCOMPRESSED,NULL)); OPENSSL_free(s);
+			printf("OP_kP: \n");printf("%s\n",s=EC_POINT_point2hex(ec_group,OSSL_Q,POINT_CONVERSION_UNCOMPRESSED,NULL));  OPENSSL_free(s);
+			break;
+		}
+		cnt += test;
+	}
+	printf(" %ld %s\n",cnt , cnt == TEST_TIMES? "OK" : "FAIL" );
+
+	/* Double point multiplication */
+	printf("Double point test:");
+	BN_rand_range(k,ec_order);
+	EC_POINT_mul(ec_group,OSSL_kG,k,NULL,NULL,NULL);
+	cnt = 0;
+	for(i=0;i<TEST_TIMES;i++)
+	{
+		BN_rand_range(k,ec_order);
+		BN_rand_range(k1,ec_order);
+		EC_POINT_mul(ec_group,OSSL_Q,k,OSSL_kG,k1,NULL);
+
+		to_littleEndian(str_k,k);
+		to_littleEndian(str_k1,k1);
+		to_point(&P,ec_group,OSSL_kG);
+		double_point_multiplication(&kG,str_k,str_k1,&P);
+		test = compare_point(ec_group,OSSL_Q,&kG);
+		if(!test)
+		{
+			STR_BYTES buf,buf1;
+			char* s = NULL;
+			to_littleEndian(buf,k);
+			to_littleEndian(buf1,k1);
+			printf("TH_k : \n");print_str_bytes(str_k);
+			printf("TH_k1: \n");print_str_bytes(str_k1);
+			printf("TH_P : \n");print_affine_1way(&P);
+			printf("TH_kP: \n");print_affine_1way(&kG);
+			printf("OP_k : \n");print_str_bytes(buf);
+			printf("OP_k1: \n");print_str_bytes(buf1);
+			printf("OP_P : \n");printf("%s\n",s=EC_POINT_point2hex(ec_group,OSSL_kG,POINT_CONVERSION_UNCOMPRESSED,NULL)); OPENSSL_free(s);
+			printf("OP_kP: \n");printf("%s\n",s=EC_POINT_point2hex(ec_group,OSSL_Q,POINT_CONVERSION_UNCOMPRESSED,NULL));  OPENSSL_free(s);
+			break;
+		}
+		cnt += test;
+	}
+	printf(" %ld %s\n",cnt , cnt == TEST_TIMES? "OK" : "FAIL" );
+
+
+	EC_POINT_free(OSSL_Q);
 	EC_POINT_free(OSSL_kG);
 	BN_free(k);
+	BN_free(k1);
 	BN_free(ec_order);
 	EC_KEY_free(ec_key);
-
 }
